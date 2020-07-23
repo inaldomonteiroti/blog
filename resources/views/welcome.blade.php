41bdf1a978d75769164571165ce69bdab0ec9eaf
Inaldo Monteiro <inaldomonteiro@localhost.localdomain>
2020-07-23 19:49:41 -0300

    Utilizando método de autenticação

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,12 +93,12 @@
                 <div>
                     <h2>Teste com Rotas:</h2>
                     <form action="/contato" method="post">
-                        {{ csrf_field()  }}
+                        {{ @csrf  }}
                         <input type="text" name="nome" placeholder="Nome / POST">
                         <button>Enviar</button>
                     </form>
                     <form action="/contato" method="post">
-                        {{ csrf_field()  }}
+                        {{ @csrf }}
                         <input type="hidden" name="_method" value="put">
                         <input type="text" name="nome" placeholder="Nome / PUT">
                         <button>Enviar</button>
